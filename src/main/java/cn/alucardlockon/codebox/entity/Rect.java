9bd3aa3bf3db599b6af34ec96df6f9c218960c54
alucardlockon <alucardlockon3@gmail.com>
2020-08-10 22:10:24 +0800

    entity

---
+++ src/main/java/cn/alucardlockon/codebox/entity/Rect.java
@@ -0,0 +1,43 @@
+package cn.alucardlockon.codebox.entity;
+
+public class Rect {
+    private String x;
+
+    private String y;
+
+    private String width;
+
+    private String heght;
+
+    public String getX() {
+        return x;
+    }
+
+    public void setX(String x) {
+        this.x = x;
+    }
+
+    public String getY() {
+        return y;
+    }
+
+    public void setY(String y) {
+        this.y = y;
+    }
+
+    public String getWidth() {
+        return width;
+    }
+
+    public void setWidth(String width) {
+        this.width = width;
+    }
+
+    public String getHeght() {
+        return heght;
+    }
+
+    public void setHeght(String heght) {
+        this.heght = heght;
+    }
+}
